f2981dc480ea25e613ff9e652aa04e2a4280708d
GoYabai <leduchuyqazzaq@gmail.com>
2026-04-21 11:26:13 +0700

    More gradient de tem

---
+++ math/Implement_Gradient_Descent_for_a_1D_Quadratic.py
@@ -0,0 +1,11 @@
+def gradient_descent_quadratic(a, b, c, x0, lr, steps):
+    """
+    Return final x after 'steps' iterations.
+    """
+    x = x0
+    for _ in range(steps):
+        x = x - lr*des(a,b,c,x)
+    return x
+
+def des(a, b, c, x):
+    return 2*a*x + b
