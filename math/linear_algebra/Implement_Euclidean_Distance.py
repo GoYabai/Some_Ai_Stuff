e4852130e8081ab1022b7890cf4c6c2484536896
GoYabai <leduchuyqazzaq@gmail.com>
2026-06-16 10:10:53 +0700

    Add Euclidean Distance

---
+++ math/linear_algebra/Implement_Euclidean_Distance.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def euclidean_distance(x, y):
+    """
+    Compute the Euclidean (L2) distance between vectors x and y.
+    Must return a float.
+    """
+    # Write code here
+    x = np.array(x)
+    y = np.array(y)
+    z = (x - y)**2
+    nova = np.sum(z)
+    nova = np.sqrt(nova)
+    return float(nova)
