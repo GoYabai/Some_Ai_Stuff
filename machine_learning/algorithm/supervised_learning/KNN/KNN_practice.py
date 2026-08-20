ce58eabbc4c346d6a4bfa13265f709849eacecc9
GoYabai <leduchuyqazzaq@gmail.com>
2026-08-20 14:18:09 +0700

    fix

--- machine_learning/algorithm/supervised_learning/KNN/KNN_practice.py
+++ machine_learning/algorithm/supervised_learning/KNN/KNN_practice.py
@@ -1,7 +1,6 @@
+
 import numpy as np
-import matplotlib.pyplot as plt
-from collections import Counter
-from sklearn.neighbors import KNeighborsClassifier, KDTree
+from sklearn.neighbors import KDTree, KNeighborsClassifier
 
 # ==========================================
 # PART 1: DATA PREPARATION
